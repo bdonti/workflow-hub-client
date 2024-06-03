de51bc24095409733e403b60e3dff197d25e8e79
rahidhaque <ontirahid@yahoo.com>
2024-06-03 06:00:59 +0600

    About us width fixed

--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -3,7 +3,7 @@ const AboutUs = () => {
   return (
     <div>
       <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center px-4 lg:px-0">
-        <div className="space-y-4 lg:w-[700px]">
+        <div className="space-y-4 lg:w-[600px]">
           <p className="text-sm text-[#FF78BA]">Who are we</p>
           <h3 className="text-2xl lg:text-4xl text-[#353B6E] font-bold">
             We are here to improve your business by keeping track of employees.
