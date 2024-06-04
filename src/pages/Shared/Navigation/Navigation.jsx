2e964b854e4a46c9c744d9628c0501b794714c91
rahidhaque <ontirahid@yahoo.com>
2024-06-04 21:35:16 +0600

    Logout Functionality implemented in navbar

--- src/pages/Shared/Navigation/Navigation.jsx
+++ src/pages/Shared/Navigation/Navigation.jsx
@@ -1,25 +1,58 @@
 import { Button, Navbar } from "flowbite-react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Navigation = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div>
       <Navbar fluid rounded>
         <Navbar.Brand href="https://flowbite-react.com"></Navbar.Brand>
         <div className="flex md:order-2 space-x-4">
-          <Link to="/login">
-            <Button>Login</Button>
-          </Link>
+          {user ? (
+            <>
+              <div>
+                <Button onClick={handleLogOut} className="bg-[#0c0833]">
+                  Logout
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex gap-8">
+                <div>
+                  <Link to="/login">
+                    <Button className="bg-[#0c0833]">Login</Button>
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/login">
+                    <Button gradientMonochrome="teal">Register Now</Button>
+                  </Link>
+                </div>
+              </div>
+            </>
+          )}
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link href="#" active>
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="#">About</Navbar.Link>
-          <Navbar.Link href="#">Services</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Contact</Navbar.Link>
+          <Link to="/">
+            <Navbar.Link>Home</Navbar.Link>
+          </Link>
+          <Link to="/">
+            <Navbar.Link>Dashboard</Navbar.Link>
+          </Link>
+          <Link to="/">
+            <Navbar.Link>Contact Us</Navbar.Link>
+          </Link>
         </Navbar.Collapse>
       </Navbar>
     </div>
