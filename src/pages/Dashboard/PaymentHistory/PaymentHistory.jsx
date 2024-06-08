ba92fe715a80046c33579b289911a8d394c2bfe3
rahidhaque <ontirahid@yahoo.com>
2024-06-09 05:55:01 +0600

    data loaded

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,13 +1,7 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "flowbite-react";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { useContext } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { AuthContext } from "../../../providers/AuthProvider";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const PaymentHistory = () => {
   const { user } = useContext(AuthContext);
@@ -26,42 +20,29 @@ const PaymentHistory = () => {
     12: "December",
   };
 
-  const fetchPayments = async ({ pageParam }) => {
+  const fetchPayments = async ({ pageParam = 0 }) => {
     const res = await fetch(
-      `http://localhost:5000/payments/?email=${user.email}&_page=${pageParam}`
+      `http://localhost:5000/payments/?email=${user.email}&limit=5&offset=${pageParam}`
     );
     return res.json();
   };
 
-  const { data, isLoading, isError } = useInfiniteQuery({
-    queryKey: ["payments", user?.email],
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["payments"],
     queryFn: fetchPayments,
-    initialPageParam: 1,
     getNextPageParam: (lastPage) => {
-      return lastPage;
+      if (lastPage.prevOffset + 5 > lastPage.totalPayments) {
+        return false;
+      }
+      return lastPage.prevOffset + 5;
     },
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error fetching data</div>;
+  console.log(data);
 
-  const content = data?.pages.map((payments) => {
-    return payments.map((payment, idx) => (
-      <TableRow
-        key={payment._id}
-        payment={payment}
-        className="bg-white dark:border-gray-700 dark:bg-gray-800"
-      >
-        <TableCell className="text-[#353B6E]">{idx + 1}</TableCell>
-        <TableCell className="text-[#353B6E]">
-          {monthNames[payment.month]} {payment.year}
-        </TableCell>
-        <TableCell className="text-[#353B6E]">
-          {payment.transactionId}
-        </TableCell>
-      </TableRow>
-    ));
-  });
+  const payments = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.payments];
+  }, []);
 
   return (
     <div className="mt-10 mx-5 lg:mx-0">
@@ -76,16 +57,45 @@ const PaymentHistory = () => {
         </div>
       </div>
       <div className="overflow-x-auto">
-        <Table className="min-w-full divide-y divide-gray-200">
-          <TableHead>
-            <Table.HeadCell>#</Table.HeadCell>
-            <Table.HeadCell>Month</Table.HeadCell>
-            <Table.HeadCell>Transaction Id</Table.HeadCell>
-          </TableHead>
-          <TableBody className="bg-white divide-y divide-gray-200">
-            {content}
-          </TableBody>
-        </Table>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <InfiniteScroll
+            dataLength={payments ? payments.length : 0}
+            next={() => fetchNextPage()}
+            hasMore={hasNextPage}
+            loading={<div>Loading...☝️</div>}
+          >
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    #
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Month
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Transaction Id
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {payments &&
+                  payments.map((payment, idx) => (
+                    <tr
+                      key={payment._id}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    >
+                      <td className="px-6 py-4">{idx + 1}</td>
+                      <td className="px-6 py-4">
+                        {monthNames[payment.month]} {payment.year}
+                      </td>
+                      <td className="px-6 py-4">{payment.transactionId}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </InfiniteScroll>
+        </div>
       </div>
     </div>
   );
